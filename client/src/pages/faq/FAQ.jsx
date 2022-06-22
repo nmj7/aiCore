3e96d57244ec19f8cd652e6f38ae3568a019da55
Anoushka Chakravorty <73027822+Anuka04@users.noreply.github.com>
2022-06-22 22:04:52 +0530

    integrated news,research and home (#1)
    * integrated news,research and home
    
    * styling fixed
    
    Co-authored-by: TJ202 <72973488+TJ202@users.noreply.github.com>

---
+++ client/src/pages/faq/FAQ.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import FAQP from "./FAQP";
+import "./faq.css";
+function FAQ() {
+  const [faqs, setfaqs] = useState([
+    {
+      question: "How do you get started with coding on AI/ML?",
+      answer:
+        "Pick a topic you are interested in, Find a quick solution, Improve your simple solution, Share your solution and Repeat the above for different algorithms.",
+      open: false,
+    },
+    {
+      question:
+        "I would like to edit my articles. How do I make a good edit that will improve the quality of the post?",
+      answer:
+        "Make your edits as complete as possible and DO NOT edit spam posts. Additionally, do not edit code in questions unless you know exactly what you are doing. Lastly, write a relevant Edit Summary and avoid putting unnesscary tags in your articles.",
+      open: false,
+    },
+    {
+      question: "Is it okay to ask for code optimization help?",
+      answer:
+        "Consider what it is you are asking. If you have actually profiled the code, have specific snippets so that everyone can run the same code to see its performance, and you have this library publicly published somewhere, like GitHub, Bitbucket, or another public facing code repository, then asking if Code Review is acceptable. If you are just starting the code but have profiled an exact snippet that exhibits the aberrant performance, then asking it on Stack Overflow is acceptable. Per the above, including the results from your profiler of choice with identified bottlenecks would go a long way towards keeping it on topic on either site. However, if you are looking for someone to help you optimize the code without having done any of the due diligence mentioned prior, reconsider posting your question. It is important that you demonstrate why you believe that your piece of code is not performant, as well as provide as much evidence as you can to back it up.",
+      open: false,
+    },
+    {
+      question: "How to ask a good question on a specific topic?",
+      answer:
+        "Do Your Research (i.e. do not be a duplicate), Generalize Everything, Detail Your Efforts (with code) and Keep the Pleasantries Out of it. Finally, Market Your Question Correctly with the help of tags",
+      open: false,
+    },
+    {
+      question: "Do you provide videos on AI/ML coding and content?",
+      answer: "We are working on it at the moment. They will be provided soon.",
+      open: false,
+    },
+  ]);
+  const toggleFAQ = (index) => {
+    setfaqs(
+      faqs.map((faq, i) => {
+        if (i === index) {
+          faq.open = !faq.open;
+        } else {
+          faq.open = false;
+        }
+
+        return faq;
+      })
+    );
+  };
+  return (
+    <div className="App">
+      <div className="contentF">
+        <h1>FAQ</h1>
+      </div>
+      <div className="faqs">
+        {faqs.map((faq, i) => (
+          <FAQP faq={faq} index={i} toggleFAQ={toggleFAQ} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default FAQ;
---
+++ client/src/pages/research/Research.js
@@ -0,0 +1,227 @@
+import React from "react";
+import "./researchItem.css";
+import arxiv from "arxiv-api";
+
+const Research = () => {
+  const papers = arxiv
+    .search({
+      searchQueryParams: [
+        {
+          include: [
+            { name: "Machine Learning" },
+            { name: "Artificial Intelligence" },
+          ],
+        },
+      ],
+      start: 0,
+      maxResults: 10,
+    })
+    .then((paper) => {
+      console.log(paper[0]);
+      console.table(paper);
+      //show titles and summaries
+      document.getElementById("titl1").innerHTML = paper[0].title;
+      document.getElementById("des1").innerHTML = paper[0].summary;
+      document.getElementById("auth1").innerHTML =
+        "Author: " + paper[0].authors;
+      document.getElementById("titl1").href = paper[0].links[1].href;
+      console.log(paper[0].links[1].href);
+
+      document.getElementById("titl2").innerHTML = paper[1].title;
+      document.getElementById("titl2").href = paper[1].links[1].href;
+      document.getElementById("des2").innerHTML = paper[1].summary;
+      document.getElementById("auth2").innerHTML =
+        "Author: " + paper[1].authors;
+      document.getElementById("titl3").innerHTML = paper[2].title;
+      document.getElementById("titl3").href = paper[2].links[1].href;
+      document.getElementById("des3").innerHTML = paper[2].summary;
+      document.getElementById("auth3").innerHTML =
+        "Author: " + paper[2].authors;
+      document.getElementById("titl4").innerHTML = paper[3].title;
+      document.getElementById("titl4").href = paper[3].links[1].href;
+      document.getElementById("des4").innerHTML = paper[3].summary;
+      document.getElementById("auth4").innerHTML =
+        "Author: " + paper[3].authors;
+      document.getElementById("titl5").innerHTML = paper[4].title;
+      document.getElementById("titl5").href = paper[4].links[1].href;
+      document.getElementById("des5").innerHTML = paper[4].summary;
+      document.getElementById("auth5").innerHTML =
+        "Author: " + paper[4].authors;
+      document.getElementById("titl6").innerHTML = paper[5].title;
+      document.getElementById("titl6").href = paper[5].links[1].href;
+      document.getElementById("des6").innerHTML = paper[5].summary;
+      document.getElementById("auth6").innerHTML =
+        "Author: " + paper[5].authors;
+      document.getElementById("titl7").innerHTML = paper[6].title;
+      document.getElementById("titl7").href = paper[6].links[1].href;
+      document.getElementById("des7").innerHTML = paper[6].summary;
+      document.getElementById("auth7").innerHTML =
+        "Author: " + paper[6].authors;
+      document.getElementById("titl8").innerHTML = paper[7].title;
+      document.getElementById("titl8").href = paper[7].links[1].href;
+      document.getElementById("des8").innerHTML = paper[7].summary;
+      document.getElementById("auth8").innerHTML =
+        "Author: " + paper[7].authors;
+      document.getElementById("titl9").innerHTML = paper[8].title;
+      document.getElementById("titl9").href = paper[0].links[1].href;
+      document.getElementById("des9").innerHTML = paper[8].summary;
+      document.getElementById("auth9").innerHTML =
+        "Author: " + paper[8].authors;
+    });
+  return (
+    <div>
+      <div className="contentR">
+        <h1>Enhance your knowledge!</h1>
+        <h2 className="h">Check out these papers!</h2>
+      </div>
+      {/* {Object.entries(papers).forEach((paper) => { */}
+      {/* return <ResearchItem title={paper.title} description={paper.summary} />; */}
+      {/* })} */}
+      <div>
+        <div className="r-app">
+          <div className="r-item">
+            <br />
+            <h2>
+              {/* <a href={url} class="res" target="_blank"> */}
+              <a class="res" id="titl1" target="_blank"></a>
+            </h2>
+            <br />
+            <p class="des" id="des1"></p>
+            <br />
+            <h4 class="auth" id="auth1"></h4>
+          </div>
+        </div>
+
+        <div>
+          <div className="r-app">
+            <div className="r-item">
+              <br />
+              <h2>
+                {/* <a href={url} class="res" target="_blank"> */}
+                <a class="res" href="/" id="titl5" target="_blank"></a>
+              </h2>
+              <br />
+              <p class="des" id="des5"></p>
+              <br />
+              <h4 class="auth" id="auth5"></h4>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="r-app">
+            <div className="r-item">
+              <br />
+              <h2>
+                {/* <a href={url} class="res" target="_blank"> */}
+                <a class="res" href="/" id="titl2" target="_blank"></a>
+              </h2>
+              <br />
+              <p class="des" id="des2"></p>
+              <br />
+              <h4 class="auth" id="auth2"></h4>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="r-app">
+            <div className="r-item">
+              <br />
+              <h2>
+                {/* <a href={url} class="res" target="_blank"> */}
+                <a class="res" href="/" id="titl4" target="_blank"></a>
+              </h2>
+              <br />
+              <p class="des" id="des4"></p>
+              <br />
+              <h4 class="auth" id="auth4"></h4>
+            </div>
+          </div>
+        </div>
+        
+        <div>
+          <div className="r-app">
+            <div className="r-item">
+              <br />
+              <h2>
+                {/* <a href={url} class="res" target="_blank"> */}
+                <a class="res" href="/" id="titl3" target="_blank"></a>
+              </h2>
+              <br />
+              <p class="des" id="des3"></p>
+              <br />
+              <h4 class="auth" id="auth3"></h4>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="r-app">
+            <div className="r-item">
+              <br />
+              <h2>
+                {/* <a href={url} class="res" target="_blank"> */}
+                <a class="res" href="/" id="titl6" target="_blank"></a>
+              </h2>
+              <br />
+              <p class="des" id="des6"></p>
+              <br />
+              <h4 class="auth" id="auth6"></h4>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="r-app">
+            <div className="r-item">
+              <br />
+              <h2>
+                {/* <a href={url} class="res" target="_blank"> */}
+                <a class="res" href="/" id="titl7" target="_blank"></a>
+              </h2>
+              <br />
+              <p class="des" id="des7"></p>
+              <br />
+              <h4 class="auth" id="auth7"></h4>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="r-app">
+            <div className="r-item">
+              <br />
+              <h2>
+                {/* <a href={url} class="res" target="_blank"> */}
+                <a class="res" href="/" id="titl8" target="_blank"></a>
+              </h2>
+              <br />
+              <p class="des" id="des8"></p>
+              <br />
+              <h4 class="auth" id="auth8"></h4>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="r-app">
+            <div className="r-item">
+              <br />
+              <h2>
+                {/* <a href={url} class="res" target="_blank"> */}
+                <a class="res" href="/" id="titl9" target="_blank"></a>
+              </h2>
+              <br />
+              <p class="des" id="des9"></p>
+              <br />
+              <h4 class="auth" id="auth9"></h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Research;
